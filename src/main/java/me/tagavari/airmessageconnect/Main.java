c1d0059dfa4ad054affec3a51ed234f3871b3d3d
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-04-14 16:54:15 -0400

    Version change to 1.0.3

--- src/main/java/me/tagavari/airmessageconnect/Main.java
+++ src/main/java/me/tagavari/airmessageconnect/Main.java
@@ -22,7 +22,7 @@ import java.util.Date;
 import java.util.logging.*;
 
 public class Main {
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	private static final int port = 1259;
 	
 	private static final File logFile = new File("logs", "latest.log");
